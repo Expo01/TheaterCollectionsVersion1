f38fd9a5a6076d941966d7a883a240a952e4529d
Expo <crdailey01@gmail.com>
2023-02-22 20:22:07 -0800

    use of Comparable Interface with overriden compareTo method with Collection.binarySearch method to more efficiently search for an item in a list. Demonstration generic variable type with interchangability of subclass object type

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-	    Theatre theatre = new Theatre("Olympian", 3, 5);
-        theatre.getSeats();
-        if(theatre.reserveSeat("B02")) {
+	    Theatre theatre = new Theatre("Olympian", 8, 12);
+//        theatre.getSeats();
+        if(theatre.reserveSeat("A01")) {
             System.out.println("Please pay");
         } else {
             System.out.println("Sorry, seat is taken");
         }
-        if(theatre.reserveSeat("B2")) {
+        if(theatre.reserveSeat("B02")) {
             System.out.println("Please pay");
         } else {
             System.out.println("Sorry, seat is taken");
--- src/Theatre.java
+++ src/Theatre.java
@@ -6,6 +6,14 @@ import java.util.*;
 public class Theatre {
     private final String theatreName;
     private List<Seat> seats = new ArrayList<>(); //generic List Class used for collection of Seat objects
+//    private Collection<Seat> seats = new ArrayList<>(); //can make this more generic by using a more generic variable type
+    // but still using the same object type. Using an ArrayList is horribly inefficient here, since, supposing I was at
+    //the very last row in the last seat, the code had to loop through all other items before reaching the seat
+
+//    private Collection<Seat> seats = new LinkedList<>(); //can do linked list as object type instead and it still works
+    // under the collections framework. Can also do this with HashSet and LinkedHashset
+
+
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
@@ -35,21 +43,38 @@ public class Theatre {
     }
 
     public boolean reserveSeat(String seatNumber) {
-        Seat requestedSeat = null;
-        for(Seat seat : seats) {
-            if(seat.getSeatNumber().equals(seatNumber)) { //could make this code better by not requiring user to input
-                // leading zero for single digit number, but whatever. Not the point here.
-                requestedSeat = seat;
-                break;
-            }
-        }
-
-        if(requestedSeat == null) {
-            System.out.println("There is no seat " + seatNumber);
+//        Seat requestedSeat = null;
+        Seat requestedSeat = new Seat(seatNumber); //strange that a new Seat is 'created' to check if that object actually
+        //exists in the collection.
+        int foundSeat = Collections.binarySearch(seats,requestedSeat,null);
+        //binary search divides list into 2 groups and determines if search item is "below" or "above" the middle item which divides the groups
+        //this is extremely fast. Returns the number of the item in the collection, such that 'A01' would return '1'
+        System.out.println(foundSeat + " is found seat value ");
+        if (foundSeat >= 0) {
+            return seats.get(foundSeat).reserve(); //uses found # of item in collection, modifies boolean field of Seat object and returns it
+        } else {
+            System.out.println("there is no seat " + seatNumber);
             return false;
         }
 
-        return requestedSeat.reserve();
+ //THIS CONTENT IS ONLY REQUIRED WITHOUT THE COMPARABLE INTERFACE
+//        for(Seat seat : seats) {
+//            System.out.print("."); //note that .print vs .println means all on same line. Used to show how inefficient
+//            // this code is by the number of iterations through the loop it takes to find the desired seat.
+//            if(seat.getSeatNumber().equals(seatNumber)) { //could make this code better by not requiring user to input
+//                // leading zero for single digit number, but whatever. Not the point here.
+//                requestedSeat = seat;
+//                break;
+//            }
+//        }
+//
+//        if(requestedSeat == null) {
+//            System.out.println("There is no seat " + seatNumber);
+//            return false;
+//        }
+//
+//        return requestedSeat.reserve();'
+
     }
 
     // for testing
@@ -59,13 +84,21 @@ public class Theatre {
         }
     }
 
-    private class Seat { // Seat is appropriately an Inner Class of Theater, since having a Seat with no Theater is out of concept
+    private class Seat implements Comparable<Seat>{ // Seat is appropriately an Inner Class of Theater, since having a Seat with no Theater is out of concept
         // Seat objects are only generated with initial generation of Theater object
+        //with the addition of the Comparable interfacee and overriding its compareTo method, the efficiency of searching is increased/
+        // not entirely sure how, but the Collections.binarySearch method needs this search object to have the Comparable interface, so
+        // just going to accept it for now
         private final String seatNumber;
         private boolean reserved = false;
 
         public Seat(String seatNumber) {
             this.seatNumber = seatNumber;
+        }
+
+        @Override
+        public int compareTo(Seat seat) {
+            return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
         }
 
         public boolean reserve() {
